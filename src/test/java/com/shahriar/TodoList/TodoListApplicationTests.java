72a1141c263cbe86df88783c2cc6a86e0ffb7eb3
shahriar808 <shasan5525@gmail.com>
2024-09-13 22:58:09 +0600

    initial commit

---
+++ src/test/java/com/shahriar/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahriar.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
